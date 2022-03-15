478e0a5f67f1a49b92ab56bc6224b64a3f2feffc
luki99111 <eveloelo3@op.pl>
2022-03-15 17:15:58 +0100

    changes in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+    System.out.println("pierwsza linia z galezi main");
+        System.out.println("dtuga linia z galezi main");
     }
 }
